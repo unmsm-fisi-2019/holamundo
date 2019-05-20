c22a1f4def92c7dda14de9c9879d9d08b75b1aee
CarlosValdiviaEspino <50279584+CarlorD527@users.noreply.github.com>
2019-05-20 12:34:10 -0500

    Add files via upload

---
+++ Holamundo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author LaboratorioFISI
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola mundo cruel");
+        // TODO code application logic here
+    }
+    
+}
